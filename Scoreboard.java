f8d047f8ed829fbb50f81b2c76e655a032902072
Rasmus Haversen <rhaversen@gmail.com>
2024-07-22 02:53:39 +0200

    Add scoreboard

---
+++ Scoreboard.java
@@ -0,0 +1,45 @@
+import java.util.Map;
+
+public class Scoreboard {
+    private Map<Integer, Integer> points;
+    
+    public Scoreboard(int maxNumber) {
+        // Initialize the scoreboard with all numbers from 1 to 12
+        // and set the value to 0
+        for (int i = 1; i <= maxNumber; i++) {
+            this.points.put(i, 0);
+        }
+    }
+
+    public Scoreboard() {
+        this(12);
+    }
+    
+    public boolean isFull() {
+        for (int value : this.points.values()) {
+            if (value < 5) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void set(int number, int points) {
+        this.points.put(number, this.points.get(number) + points);
+    }
+
+    public int getPointsForValue(int number) {
+        return this.points.get(number);
+    }
+
+    public Map<Integer, Integer> getPoints() {
+        return this.points;
+    }
+
+    public void reset() {
+        for (int key : this.points.keySet()) {
+            this.points.put(key, 0);
+        }
+    }
+
+}
