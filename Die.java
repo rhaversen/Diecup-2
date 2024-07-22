947b048df7969bf93419447cea4e1dc834341141
Rasmus Haversen <rhaversen@gmail.com>
2024-07-22 02:41:07 +0200

    Add simple die

---
+++ Die.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class Die {
+    private int faceValue;
+    private int sides;
+    private Random random = new Random();
+    
+    public Die(int sides) {
+        this.sides = sides;
+        this.faceValue = 1;
+    }
+    
+    public void roll() {
+        this.faceValue = random.nextInt(this.sides) + 1;
+    }
+    
+    public int getFaceValue() {
+        return this.faceValue;
+    }
+    
+    public String toString() {
+        return "Die with " + this.sides + " sides, value: " + this.faceValue;
+    }
+}
