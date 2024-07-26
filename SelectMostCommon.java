1a7a87eea58610dc2a09497db2f82d76386ec9ed
Rasmus Haversen <rhaversen@gmail.com>
2024-07-26 19:02:11 +0200

    Add SelectMostCommon

---
+++ SelectMostCommon.java
@@ -0,0 +1,18 @@
+import java.util.Map;
+
+public class SelectMostCommon implements Strategy {
+    public int getSelectedNumber(Map<Integer, Integer> valueMap, Scoreboard scoreboard) {
+        int bestSum = -1;
+        int maxCount = 0; // Track the highest count found in valueMap
+
+        for (Map.Entry<Integer, Integer> entry : valueMap.entrySet()) {
+            int sum = entry.getKey();
+            int count = entry.getValue();
+            if (scoreboard.getPoints().getOrDefault(sum, 0) < 5 && count > maxCount) {
+                maxCount = count;
+                bestSum = sum;
+            }
+        }
+        return bestSum;
+    }
+}
