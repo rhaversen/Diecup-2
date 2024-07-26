3e4d1b916f9e6fc9b1085c41fcc862696f168ac2
Rasmus Haversen <rhaversen@gmail.com>
2024-07-26 02:14:04 +0200

    Fix perfect pairs algorithm, returning correct values map

--- DieCup.java
+++ DieCup.java
@@ -18,28 +18,91 @@ public class DieCup {
     }
 
     public Map<Integer, Integer> getValuesMap() {
-        HashMap<Integer, Integer> valuesMap = new HashMap<>();
-        int n = dieList.length;
+        Map<Integer, Integer> singleDieValuesMap = new HashMap<>();
+        Map<Integer, Integer> pairDieValuesMap = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            Die die1 = dieList[i];
-            int faceValue1 = die1.getFaceValue();
+        // Add all values for single dice
+        for (int i = 0; i < dieList.length; i++) {
+            int faceValue = dieList[i].getFaceValue();
+            singleDieValuesMap.put(faceValue, singleDieValuesMap.getOrDefault(faceValue, 0) + 1);
+        }
 
-            // Add all single die values to the map
-            valuesMap.put(faceValue1, valuesMap.getOrDefault(faceValue1, 0) + 1);
+        // Select the first initial pair of dice that make the sum
+        for (int i = 0; i < dieList.length; i++) {
+            // The second die should be greater than the first die
+            for (int j = i + 1; j < dieList.length; j++) {
 
-            // Add all possible combinations of two dice to the map
-            // Only add with dice after i'th to avoid double counting
-            for (int j = i + 1; j < n; j++) {
-                Die die2 = dieList[j];
-                int faceValue2 = die2.getFaceValue();
-                int sum = faceValue1 + faceValue2;
-                
-                valuesMap.put(sum, valuesMap.getOrDefault(sum, 0) + 1);
+                // Create new dielist of none selcted dice
+                Die[] newDieList = new Die[dieList.length - 2];
+                int index = 0;
+                for (int k = 0; k < dieList.length; k++) {
+                    if (k != i && k != j) {
+                        newDieList[index++] = dieList[k];
+                    }
+                }
+
+                if (dieList[i].getFaceValue() + dieList[j].getFaceValue() > 6) {
+                    // Count the pairs for given target
+                    int pairs = 1 + countSumPairs(newDieList, dieList[i].getFaceValue() + dieList[j].getFaceValue());
+
+                    // Add it to the map if it is bigger
+                    if (pairs > pairDieValuesMap.getOrDefault(dieList[i].getFaceValue() + dieList[j].getFaceValue(), 0)) {
+                        pairDieValuesMap.put(dieList[i].getFaceValue() + dieList[j].getFaceValue(), pairs);
+                    }
+                }
+
+            }
+        }
+
+        // Merge the two maps, overwriting the single die values with the pair die values
+        Map<Integer, Integer> valuesMap = new HashMap<>();
+        for (Map.Entry<Integer, Integer> entry : singleDieValuesMap.entrySet()) {
+            valuesMap.put(entry.getKey(), entry.getValue());
+        }
+        for (Map.Entry<Integer, Integer> entry : pairDieValuesMap.entrySet()) {
+            if (entry.getValue() > valuesMap.getOrDefault(entry.getKey(), 0)) {
+                valuesMap.put(entry.getKey(), entry.getValue());
             }
         }
 
         return valuesMap;
+    }
+
+    private int countSumPairs(Die[] dieList, int target) {
+        int maxCount = 0;
+
+        // If there are no die pairs left, return the count immediately
+        if (dieList.length <= 1) {
+            return 0;
+        }
+
+        // If the target is 6 or less, return the count immediately
+        if (target <= 6) {
+            return 0;
+        }
+
+        for (int i = 0; i < dieList.length; i++) {
+            // The second die should be greater than the first die
+            for (int j = i + 1; j < dieList.length; j++) {
+
+                // Create new dielist of non selcted dice
+                Die[] newDieList = new Die[dieList.length - 2];
+                int index = 0;
+                for (int k = 0; k < dieList.length; k++) {
+                    if (k != i && k != j) {
+                        newDieList[index++] = dieList[k];
+                    }
+                }
+
+                // If the sum of the two dice is equal to the target, count remaining pairs
+                if (dieList[i].getFaceValue() + dieList[j].getFaceValue() == target) {
+                    int remainingPairs = countSumPairs(newDieList,
+                            dieList[i].getFaceValue() + dieList[j].getFaceValue());
+                    maxCount = Math.max(maxCount, 1 + remainingPairs);
+                }
+            }
+        }
+        return maxCount;
     }
 
     public void removeDice(int amount) {
