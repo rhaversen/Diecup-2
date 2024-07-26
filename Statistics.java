87dbf7e24cdff53cdb29c1617c19875001beb5e4
Rasmus Haversen <rhaversen@gmail.com>
2024-07-26 18:02:18 +0200

    Add probability calculation

---
+++ Statistics.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Statistics {
+    Map<Integer, Double> probabilities;
+
+    public Statistics(int numberOfDice, int sides) {
+        calculateProbabilities();
+    }
+
+    public Map<Integer, Double> getProbabilities() {
+        return probabilities;
+    }
+
+    private void calculateProbabilities() {
+        int iterations = 10000;
+        Map<Integer, Integer> result = new HashMap<>();
+
+        for (int i = 0; i < iterations; i++) {
+            DieCup dieCup = new DieCup(6, 6);
+            Map<Integer, Integer> valuesMap = dieCup.getValuesMap();
+
+            // Add the valuesMap to the result
+            for (int key : valuesMap.keySet()) {
+                int preResult = result.getOrDefault(key, 0);
+                int newValue = valuesMap.get(key);
+                int newResult = preResult + newValue;
+                result.put(key, newResult);
+            }
+
+        }
+
+        // Create a new map to store normalized values
+        Map<Integer, Double> normalizedMap = new HashMap<>();
+
+        // Normalize each value
+        for (Map.Entry<Integer, Integer> entry : result.entrySet()) {
+            normalizedMap.put(entry.getKey(), entry.getValue() / (double) iterations);
+        }
+        probabilities = normalizedMap;
+    }
+}
