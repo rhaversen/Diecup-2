bc00f917f2a40d7b1912912c0ea0c5b29371978d
Rasmus Haversen <rhaversen@gmail.com>
2024-07-27 02:25:31 +0200

    Add default game loop to main method

--- Main.java
+++ Main.java
@@ -1,4 +1,12 @@
+import diecup.Game;
+import strategies.WeightedSelect;
+
 public class Main {
     public static void main(String[] args) {
+        int numberOfDice = 6;
+        int sides = 6;
+        Game game = new Game(numberOfDice, sides, new WeightedSelect(numberOfDice, sides));
+        game.startGame();
     }
 }
+
